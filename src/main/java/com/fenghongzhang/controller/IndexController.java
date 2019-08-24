16bafa96b6d52495a3857b4fd28a7e7089ece318
304978499@qq.com <304978499@qq.com>
2019-08-24 23:35:35 +0800

    test

--- src/main/java/com/fenghongzhang/controller/IndexController.java
+++ src/main/java/com/fenghongzhang/controller/IndexController.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * 首页controller
+ * @author fhz
+ *
+ */
 @Controller
 public class IndexController {
 
+	//返回首页
 	@GetMapping("/index")
 	public String getIndex(Model model) {
 		return "index";
